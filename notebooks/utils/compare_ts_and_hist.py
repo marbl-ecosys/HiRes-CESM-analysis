b7ec1a07dc66b741913adff5bf9c1cb42c335609
Michael Levy <mike.levy.work@gmail.com>
2020-10-30 10:12:42 -0600

    Bugfix in how CaseClass is used
    compare_ts_and_hist.py needs CaseClass.CaseClass, not just CaseClass

--- notebooks/utils/compare_ts_and_hist.py
+++ notebooks/utils/compare_ts_and_hist.py
@@ -29,7 +29,7 @@ def compare_ts_and_hist(
 
     found_all = True
 
-    case = CaseClass(casename)
+    case = CaseClass.CaseClass(casename)
     # Return if no time series is available
     if len(case.get_timeseries_files(year, stream)) == 0:
         return "no time series"
